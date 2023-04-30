718e789227f329a0bd1b75e300d68de8eece3bdc
ajay <akverma@caastle.com>
2023-04-30 10:21:42 +0530

    Added instances of Bird, only problem is penguin doesn't fly

--- src/main/java/org/learning/pakshi/Bird.java
+++ src/main/java/org/learning/pakshi/Bird.java
@@ -5,33 +5,19 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class Bird {
+public abstract class Bird {
     // Step 1 - add attributes
-    private String name;
     private Double weight;
-    private String color;
     private Double size;
+    private String color;
     private String beakType;
-    private BirdType type;
 
     // Step 2 - Add behaviors
-    public void fly() {
-        switch (type) {
-            case PARROT -> System.out.println("Parrot is flying");
-            case PENGUIN -> System.out.println("Penguin is not flying");
-            case SPARROW -> System.out.println("Sparrow is flying");
-        }
+    public abstract void fly();
 
-        throw new RuntimeException("Invalid bird type " + type);
-    }
+    public abstract void makeSound();
 
-    public void makeSound() {
-        switch (type) {
-            case PARROT -> System.out.println("Parrot is making a sound");
-            case PENGUIN -> System.out.println("Penguin is making a sound");
-            case SPARROW -> System.out.println("Sparrow is making a sound");
-        }
-
-        throw new RuntimeException("Invalid bird type " + type);
+    public void eating() {
+        System.out.println("Bird is eating...");
     }
 }
