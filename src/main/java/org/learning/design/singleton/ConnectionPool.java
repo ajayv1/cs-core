439955bafee0e1e1e4dc0831bb61e5e0a65f745c
ajay <akverma@caastle.com>
2023-05-05 10:55:17 +0530

    Singleton thread safe version

--- src/main/java/org/learning/design/singleton/ConnectionPool.java
+++ src/main/java/org/learning/design/singleton/ConnectionPool.java
@@ -2,14 +2,17 @@ package org.learning.design.singleton;
 
 public class ConnectionPool {
     // Step 3 - instance variable
-    private static ConnectionPool instance = new ConnectionPool();
+    private static ConnectionPool instance = null;
 
     // step 1: hide constructor
     private ConnectionPool() {}
 
     // step 2: global access point
-    public static ConnectionPool getInstance() {
+    public static synchronized ConnectionPool getInstance() {
         // step 4 - return instance
+        if (instance == null) {
+            instance = new ConnectionPool();
+        }
         return instance;
     }
 }
