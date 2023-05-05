1e9d76365476b065fc8c3790ebc9747266f97e09
ajay <akverma@caastle.com>
2023-05-05 10:49:25 +0530

    Singleton eager loaded pattern

--- src/main/java/org/learning/design/singleton/ConnectionPool.java
+++ src/main/java/org/learning/design/singleton/ConnectionPool.java
@@ -2,19 +2,14 @@ package org.learning.design.singleton;
 
 public class ConnectionPool {
     // Step 3 - instance variable
-    private static ConnectionPool instance;
+    private static ConnectionPool instance = new ConnectionPool();
 
     // step 1: hide constructor
     private ConnectionPool() {}
 
     // step 2: global access point
     public static ConnectionPool getInstance() {
-        // step 4 - if instance present return, else create, set and return
-        if (instance == null) {
-            instance = new ConnectionPool();
-        }
-
+        // step 4 - return instance
         return instance;
     }
-
 }
