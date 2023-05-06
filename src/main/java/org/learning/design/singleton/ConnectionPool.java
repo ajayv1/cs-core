8abc358f3716a51c8ddcb3e0c501f04b1bf31b12
ajay <akverma@caastle.com>
2023-05-06 07:23:29 +0530

    Added double-checked locking mechanism for singleton

--- src/main/java/org/learning/design/singleton/ConnectionPool.java
+++ src/main/java/org/learning/design/singleton/ConnectionPool.java
@@ -8,11 +8,28 @@ public class ConnectionPool {
     private ConnectionPool() {}
 
     // step 2: global access point
+
+    // synchronized makes it thread-safe, but it has performance issue as only one thread would be able to call it.
+    // so moving the synchronized to a synchronized block
+    /*
     public static synchronized ConnectionPool getInstance() {
         // step 4 - return instance
         if (instance == null) {
             instance = new ConnectionPool();
         }
+        return instance;
+    }
+    */
+    public static ConnectionPool getInstance() {
+        if (instance == null) { // first check is to
+            synchronized (ConnectionPool.class) {
+                if (instance == null) {
+                    instance = new ConnectionPool();
+                }
+            }
+        }
+        // step 4 - return instance
+
         return instance;
     }
 }
